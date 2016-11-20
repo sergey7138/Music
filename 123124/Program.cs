d55de8e351f882121cfc7066fa928a04e6b063ee
sergey7138 <sergey7138@tut.by>
2016-11-20 12:46:21 +0300

    first music by sergey7138

---
+++ 123124/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace _123124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer myPlayer = new System.Media.SoundPlayer();            
+            myPlayer.SoundLocation = @"C:\1118.wav";
+            myPlayer.PlaySync();
+           
+        }
+    }
+}
